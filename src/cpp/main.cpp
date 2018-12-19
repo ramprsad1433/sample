bf63505415540e3db5574dad9d4e745acad3cf3a
ramprasad1433 <mcitpram1433@gmail.com>
2018-12-19 10:45:04 +0530

    main

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,7 +1,1 @@
-this is first program
-in sample folder
-
-089cd37a178e478a92349c3b3aa2fed4
-
-Manifest-Version: 1.0
-Created-By: 1.5.0_03 (Sun Microsystems Inc.)
+I am starting code
--- src/cpp/sms.cpp
+++ src/cpp/sms.cpp
@@ -1,2 +1,1 @@
-This is second program
-This is starting program
+starting writing code
